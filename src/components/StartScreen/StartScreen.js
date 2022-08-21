f73aea2be881db44e1ad714d1df007754e79f9b9
machado001 <machadowg@gmail.com>
2022-08-21 02:22:10 -0300

    arrumando imagem do StartScreen

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -1,10 +1,11 @@
 import './StartScreen.css'
+import heisenberg from "../assets/heisenberg.png"
 
 const StartScreen = ({ goToGame, goToGuide }) => {
     return (
         <div className='start-screen'>
             <h1>Math.quiz()</h1>
-            <img id='start-img' src="/heisenberg.png" alt="Walter Fucking White referências" />
+            <img id='start-img' src={heisenberg} alt="Walter Fucking White referências" />
            <div>
                 <button onClick={goToGuide}>Guia</button>
                 <button onClick={goToGame} >Iniciar!</button>
